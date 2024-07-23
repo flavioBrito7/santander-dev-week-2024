2bde8d099f4368f2711c6a3aba14867428927e72
flavioBrito <flavioandersondebrito@gmail.com>
2024-07-23 06:32:06 -0300

    Iplementação da camada de domínio

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package me.dio.domain.repository;
 
-public class UserRepository {
+import me.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
 }
